700a169161a07ecfddcd2d6bd2bb0f0fff5fc690
Tien <tieeeeen1994@gmail.com>
2024-07-11 01:44:07 +0800

    Commit!

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
+# Summary
+
 Buff duration time are stackable. Drinking the same potion repeatedly now increases the duration of the buff.
+
 Note that mana sick debuff duration still has its maximum limit.
+
 It also affect furniture buffs. It doesn't affect debuffs or buffs that doesn't display duration time.
 
 Example: You drank endurance potion (4 minutes) 8 times. You got 32 minutes of endurance instead of 4 minutes.
+
 You right-clicked bewitching table (10 minutes) 144 times. You got bewitched buff for a whole day!
 
 Original Mod: https://steamcommunity.com/sharedfiles/filedetails/?id=2765534421
+
 Original Mod Author: https://steamcommunity.com/id/cyrilliy
 
 GitHub: https://github.com/tieeeeen1994/tModLoader-StackableBuffDuration
 
-Changelogs:
+# Changelogs
 
 v0.2
 - Add configuration for flexibility.
--- StackableBuffDuration2.cs
+++ StackableBuffDuration2.cs
@@ -1,15 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 
 namespace StackableBuffDuration2
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class StackableBuffDuration2 : Mod
+    public class StackableBuffDuration2 : Mod
 	{
+        public override void Load()
+        {
+            On_Player.AddBuff_TryUpdatingExistingBuffTime += TryUpdatingExistingBuffTime;
+        }
 
-	}
+        public override void Unload()
+        {
+            On_Player.AddBuff_TryUpdatingExistingBuffTime -= TryUpdatingExistingBuffTime;
+        }
+
+        private bool TryUpdatingExistingBuffTime(On_Player.orig_AddBuff_TryUpdatingExistingBuffTime orig,
+                                                 Player self, int type, int time)
+        {
+            if (time <= Config.I.buffTimeThreshold || Config.I.blacklist.Exists(buffDefinition => buffDefinition.Type == type))
+            {
+                return orig(self, type, time);
+            }
+            else
+            {
+                bool flag = false;
+                for (int i = 0; i < Player.MaxBuffs; i++)
+                {
+                    if (self.buffType[i] == type)
+                    {
+                        if (type == 94)
+                        {
+                            self.buffTime[i] = Math.Min(self.buffTime[i] + time, Player.manaSickTimeMax);
+                        }
+                        else
+                        {
+                            self.buffTime[i] += time;   
+                        }
+                        flag = true;
+                        break;
+                    }
+                }
+                return flag;
+            }
+        }
+    }
+
+    public class Config : ModConfig
+    {
+        public static Config I => ModContent.GetInstance<Config>();
+
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        public List<BuffDefinition> blacklist = [];
+        [DefaultValue(15)]
+        public int buffTimeThreshold;
+    }
 }
